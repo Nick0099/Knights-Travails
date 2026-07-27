ab47d208b6719fc0f13d3718dc0fc18c07a9c9d6
Nischal <nischalneupane45@gmail.com>
2026-07-27 21:24:23 +0545

    wrote formulae to calulate movement of the knight

--- src/Knight_travails.js
+++ src/Knight_travails.js
@@ -1,9 +1,16 @@
 const Node = (value) =>{
     const data =  value;
-    const edge1 = null;
-    const edge2 = null;
+    const posn = []
 }
 
 const Graph = () => {
- 
+    
 }
+
+/*
+movement calculations
+ to move (+)ve x = ( x + 2 , y +- 2 ) 
+ to move (-)ve x = ( x - 2 , y +- 2 ) 
+ to move (+)ve y = ( x +- 1 , y + 2 )
+ to move (-)ve y = ( x +- 1 , y - 2 )
+ */
