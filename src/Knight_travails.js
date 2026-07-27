e6fcaf3a75077f69289a0f459ec725ab52aa2d84
Nischal <nischalneupane45@gmail.com>
2026-07-27 22:18:38 +0545

    fixed basic syntax error

--- src/Knight_travails.js
+++ src/Knight_travails.js
@@ -1,20 +1,40 @@
-const Node = (value) => {
-  const data = value;
-  const posn = [];
-};
-
-const Graph = (initial = [a,b], final = [c,d]) => {
-  let steps = [];
-  let temp = [a,b];
-  while (temp != final) {
-    if(temp = steps.includes(temp)) return steps.length = 0
-
-    
-    steps.push([]); //  nests a new array in steps
-    steps[0].push(temp); //adds the step in the newly created array
+const Graph = (initial = [], final = []) => {
+  let x = initial[0];
+  let y = initial[1];
+  let xx = final[0];
+  let yy = final[1];
+  let s_steps = [];
+  let temp_steps =[];
+  let temp = [x, y];
+  let pwr = 1;
+  while (temp[0] !== xx || temp[1] !== yy) {
+    if ((temp = temp_steps.includes(temp))) return (temp_steps.length = 0);
+    switch (pwr) {
+      case (1):
+        temp = [temp[0] + 2, temp[1] + ((-1) ** pwr) * 2];
+        pwr++;
+        break;
+      case (2):
+        temp = [temp[0] - 2, temp[1] + ((-1) ** pwr) * 2];
+        pwr++;
+        break;
+      case (3):
+        temp = [temp[0] + ((-1) ** pwr) * 1, temp[1] + 2];
+        pwr++;
+        break;
+      case (4):
+        temp = [temp[0] + ((-1) ** pwr) * 1, temp[1] + 2];
+        pwr++;
+        break;
+    }
+    temp_steps.push([]); //  nests a new array in teporary steps
+    temp_steps[0].push(temp); //adds the step in the newly created array
   }
+  if(temp_steps.length < s_steps.length) temp_steps = s_steps;
+  return s_steps;
 };
 
+console.log(Graph([2, 4], [9, 2]));
 /*
 movement calculations
 
