6c7f3775426b96fd58f64818c61ac5879bd1359a
Nischal <nischalneupane45@gmail.com>
2026-07-27 21:51:15 +0545

    add logic to filter out infinite loop and storing steps

--- src/Knight_travails.js
+++ src/Knight_travails.js
@@ -1,16 +1,27 @@
-const Node = (value) =>{
-    const data =  value;
-    const posn = []
-}
+const Node = (value) => {
+  const data = value;
+  const posn = [];
+};
 
-const Graph = () => {
+const Graph = (initial = [a,b], final = [c,d]) => {
+  let steps = [];
+  let temp = [a,b];
+  while (temp != final) {
+    if(temp = steps.includes(temp)) return steps.length = 0
+
     
-}
+    steps.push([]); //  nests a new array in steps
+    steps[0].push(temp); //adds the step in the newly created array
+  }
+};
 
 /*
 movement calculations
- to move (+)ve x = ( x + 2 , y +- 2 ) 
- to move (-)ve x = ( x - 2 , y +- 2 ) 
- to move (+)ve y = ( x +- 1 , y + 2 )
- to move (-)ve y = ( x +- 1 , y - 2 )
+
+ to move (+)ve x = ( a + 2 , b +(-1 )^n * 2) ) 
+ to move (-)ve x = ( a - 2 , b +(-1 )^n * 2 ) 
+ to move (+)ve y = ( a +(-1 )^n *  1 , b + 2 )
+ to move (-)ve y = ( a +(-1 )^n *  1 , b - 2 )
+
+ where a and b are the original coordinates of the knight
  */
